5cadc5f2c0765ef05dba8ad58dac2030927998a5
Viktor <vasilevskiviktor@yahoo.com>
2025-05-14 18:49:50 +0200

    add features and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,49 @@ import { LoginComponent } from './features/login/login.component';
 import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent, canActivate: [ authGuard ] },
-    { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, canActivate: [authGuard] },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
 
-    { path: 'dashboard', 
-        loadComponent: () => import('./features/dashboard/dashboard.component').then((m) => m.DashboardComponent)
-    },
-    { path: 'categories', 
-        loadComponent: () => import('./features/categories/category-list/category-list.component').then((m) => m.CategoryListComponent)
-    },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./features/dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent
+      ),
+  },
+  {
+    path: 'categories',
+    loadComponent: () =>
+      import(
+        './features/categories/category-list/category-list.component'
+      ).then((m) => m.CategoryListComponent),
+  },
+  {
+    path: 'subcategories',
+    loadComponent: () =>
+      import(
+        './features/subcategories/subcategories/subcategories.component'
+      ).then((m) => m.SubcategoriesComponent),
+  },
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('./features/products/product-list/product-list.component').then(
+        (m) => m.ProductListComponent
+      ),
+  },
+  {
+    path: 'orders',
+    loadComponent: () =>
+      import('./features/orders/order-list/order-list.component').then(
+        (m) => m.OrderListComponent
+      ),
+  },
+  {
+    path: 'users',
+    loadComponent: () =>
+      import('./features/users/user-list/user-list.component').then(
+        (m) => m.UserListComponent
+      ),
+  },
 ];
---
+++ src/app/features/subcategories/subcategories/subcategories.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subcategories',
+  imports: [],
+  templateUrl: './subcategories.component.html',
+  styleUrl: './subcategories.component.css'
+})
+export class SubcategoriesComponent {
+
+}
