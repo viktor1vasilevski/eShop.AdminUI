44ada8df0dfb9aede8806278ef97aa9a25f6bc85
Viktor <vasilevskiviktor@yahoo.com>
2025-05-14 19:26:14 +0200

    create category layout

--- src/app/features/categories/category-create/category-create.component.ts
+++ src/app/features/categories/category-create/category-create.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-category-create',
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './category-create.component.html',
-  styleUrl: './category-create.component.css'
+  styleUrl: './category-create.component.css',
 })
 export class CategoryCreateComponent {
+  categoryName: string = '';
 
+  constructor(private router: Router) {}
+
+  createCategory() {
+    // Call your API here
+    console.log('Category Created:', this.categoryName);
+    this.router.navigate(['/categories']);
+  }
+
+  cancel() {
+    this.router.navigate(['/categories']);
+  }
 }
