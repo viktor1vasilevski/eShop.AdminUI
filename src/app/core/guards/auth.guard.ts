4fb33ae42e6c9fc22d7285a7f9fd955ed7782bff
Viktor <vasilevskiviktor@yahoo.com>
2025-05-15 23:54:43 +0200

    fix the auth guard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,23 +1,17 @@
-import { ActivatedRouteSnapshot, CanActivateFn, Router } from "@angular/router";
+import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
-import { AuthenticationManagerService } from "../services/authentication-manager.service";
+import { AuthenticationManagerService } from '../services/authentication-manager.service';
 
-
-export const authGuard: CanActivateFn = (route: ActivatedRouteSnapshot) => {
+export const authGuard: CanActivateFn = () => {
   const authenticationManagerService = inject(AuthenticationManagerService);
   const router = inject(Router);
-  //const toastr = inject(ToastrService);
 
   const isLoggedIn = authenticationManagerService.isLoggedIn();
-  
+
   if (isLoggedIn) {
-
-  }
-
-  if (route.url[0].path === 'login' || route.url[0].path === 'register') {
     return true;
+  } else {
+    router.navigate(['/unauthorized']);
+    return false;
   }
-
-  router.navigate(['/unauthorized']);
-  return false;
 };
