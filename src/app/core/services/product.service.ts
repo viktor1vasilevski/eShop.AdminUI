e468ec610c8471d22e0778ca616fb7901bf44d41
Viktor <vasilevskiviktor@yahoo.com>
2025-06-16 23:36:00 +0200

    add product list component

---
+++ src/app/core/services/product.service.ts
@@ -0,0 +1,25 @@
+import { HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DataService } from './data.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  private baseUrl = 'https://localhost:7270/api';
+  constructor(private _dataApiService: DataService) {}
+
+  getProducts(request: any): Observable<any> {
+    const params = new HttpParams()
+      .set('brand', request.name)
+      .set('subcategoryId', request.categoryId)
+      .set('skip', request.skip.toString())
+      .set('take', request.take.toString())
+      .set('sortDirection', request.sortDirection)
+      .set('sortBy', request.sortBy);
+
+    const url = `${this.baseUrl}/product`;
+    return this._dataApiService.getAll<any>(url, params);
+  }
+}
--- src/app/features/products/product-list/product-list.component.ts
+++ src/app/features/products/product-list/product-list.component.ts
@@ -1,11 +1,80 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { PaginationComponent } from '../../../core/components/pagination/pagination.component';
+import { SortOrder } from '../../../core/enums/sort-order.enum';
+import { ProductService } from '../../../core/services/product.service';
+
+export interface ProductRequest {
+  skip: number;
+  take: number;
+  sortDirection: SortOrder;
+  sortBy: string;
+  brand: string;
+}
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [CommonModule, RouterLink, FormsModule, PaginationComponent],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrl: './product-list.component.css',
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  productRequest: ProductRequest = {
+    skip: 0,
+    take: 10,
+    sortDirection: SortOrder.Descending,
+    sortBy: 'created',
+    brand: '',
+  };
 
+  products: any[] = [];
+  totalCount: number = 0;
+  totalPages: number[] = [];
+  currentPage: number = 1;
+  productToDelete: any = null;
+  constructor(public router: Router,
+    private _productService: ProductService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  loadProducts(){
+    this._productService.getProducts(this.productRequest).subscribe({
+      next: (response: any) => {
+        console.log(response);
+        
+      },
+      error: (errorResponse: any) => {
+        console.log(errorResponse);
+        
+      }
+    })
+  }
+
+  calculateTotalPages(): void {
+    const pages = Math.ceil(this.totalCount / this.productRequest.take);
+    this.totalPages = Array.from({ length: pages }, (_, i) => i + 1);
+  }
+
+  changePage(page: number): void {
+    this.currentPage = page;
+    this.productRequest.skip = (page - 1) * this.productRequest.take;
+    //this.loadCategories();
+  }
+
+  onItemsPerPageChange(itemsPerPage: number): void {
+    this.productRequest.take = itemsPerPage;
+    this.productRequest.skip = 0;
+    this.currentPage = 1;
+    //this.loadProducts();
+  }
+  onFilterChange() {}
+
+  showDeleteProductModal(product: any) {}
+
+  deleteProduct() {}
 }
