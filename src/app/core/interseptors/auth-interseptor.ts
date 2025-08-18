b525dff23eb232b791ada1a43dde4b274c3e16a3
Viktor <vasilevskiviktor@yahoo.com>
2025-08-19 00:47:49 +0200

    /

--- src/app/core/interseptors/auth-interseptor.ts
+++ src/app/core/interseptors/auth-interseptor.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+} from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
     const token = localStorage.getItem('auth_token');
     if (token) {
       const cloned = req.clone({
         setHeaders: {
-          Authorization: `Bearer ${token}`
-        }
+          Authorization: `Bearer ${token}`,
+        },
       });
       return next.handle(cloned);
     }
---
+++ src/app/core/services/order.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { DataService } from './data.service';
+import { Observable } from 'rxjs';
+import { HttpParams } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OrderService {
+  private baseUrl = 'https://localhost:44366/api';
+
+  constructor(private _dataApiService: DataService) {}
+
+  getOrders(request: any): Observable<any> {
+    const params = new HttpParams().set('userId', request.userId);
+    const url = `${this.baseUrl}/order`;
+    return this._dataApiService.getAll<any>(url, params);
+  }
+}
