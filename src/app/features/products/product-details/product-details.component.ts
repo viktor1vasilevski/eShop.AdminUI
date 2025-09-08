7bb842e257c351eca7decce662d4ecf599d78c15
Viktor <vasilevskiviktor@yahoo.com>
2025-09-09 00:21:10 +0200

    add product-details logic

--- src/app/features/products/product-details/product-details.component.ts
+++ src/app/features/products/product-details/product-details.component.ts
@@ -1,25 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ErrorHandlerService } from '../../../core/services/error-handler.service';
+import { ProductService } from '../../../core/services/product.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-details',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css',
 })
 export class ProductDetailsComponent implements OnInit {
   productId: any = '';
+  product: any;
 
   constructor(
     private route: ActivatedRoute,
-    private _errorHandlerService: ErrorHandlerService
+    private _errorHandlerService: ErrorHandlerService,
+    private _productService: ProductService
   ) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
       this.productId = params['id'];
-      console.log(this.productId);
+      this.loadProductById();
+    });
+  }
+
+  loadProductById() {
+    this._productService.getProductById(this.productId).subscribe({
+      next: (res: any) => {
+        console.log(res.data);
+        this.product = res.data;
+      },
+      error: (err: any) => this._errorHandlerService.handleErrors(err),
     });
   }
 }
