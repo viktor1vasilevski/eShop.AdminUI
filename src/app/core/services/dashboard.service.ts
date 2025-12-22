30605da1405edf82b888243bb059b50200146a22
Viktor <vasilevskiviktor@yahoo.com>
2025-12-22 19:35:20 +0100

    add dashboard logic

---
+++ src/app/core/services/dashboard.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { DataService } from './data.service';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DashboardService {
+  private baseUrl = 'https://localhost:44352/api/dashboard';
+
+  constructor(private _dataApiService: DataService) {}
+
+  getOrdersToday(): Observable<any> {
+    return this._dataApiService.getAll<any>(`${this.baseUrl}/orders-today`);
+  }
+
+  getRevenueToday(): Observable<any> {
+    return this._dataApiService.getAll<any>(`${this.baseUrl}/revenue-today`);
+  }
+
+  getTotalCustomers(): Observable<any> {
+    return this._dataApiService.getAll<any>(`${this.baseUrl}/total-customers`);
+  }
+}
--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DashboardService } from '../../core/services/dashboard.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  ordersToday = 0;
+  revenueToday = 0;
+  totalCustomers = 0;
 
+  constructor(private _dashboardService: DashboardService) {}
+
+  ngOnInit(): void {
+    this.loadKPIs();
+  }
+
+  loadKPIs() {
+    // Fetch all 3 KPIs in parallel
+    this._dashboardService.getOrdersToday().subscribe((res) => {
+      this.ordersToday = res.data?.count ?? 0;
+    });
+
+    this._dashboardService.getRevenueToday().subscribe((res) => {
+      this.revenueToday = res.data?.amount ?? 0;
+    });
+
+    this._dashboardService.getTotalCustomers().subscribe((res) => {
+      this.totalCustomers = res.data?.count ?? 0;
+    });
+  }
 }
